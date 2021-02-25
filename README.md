1e8a4f35a5868b513cf2b1a182d75b6f548c9126
Fynn Becker <fynn@hey.com>
2021-02-25 09:51:59 +0100

    feat: Setup

---
+++ README.md
@@ -0,0 +1,23 @@
+# Vue 3.0.6 Scoped Animations
+
+## Project setup
+
+```sh
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```sh
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```sh
+npm run build
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="box-unscoped"></div>
+  <div class="box-scoped"></div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+.box-unscoped {
+  width: 2em;
+  height: 2em;
+  background: #f00;
+  animation: rotate-unscoped 2s linear infinite;
+}
+
+@keyframes rotate-unscoped {
+  100% {
+    transform: rotate(1turn);
+  }
+}
+</style>
+
+<style scoped>
+.box-scoped {
+  width: 2em;
+  height: 2em;
+  background: #0f0;
+  animation: rotate-scoped 2s linear infinite;
+}
+
+@keyframes rotate-scoped {
+  100% {
+    transform: rotate(1turn);
+  }
+}
+</style>
